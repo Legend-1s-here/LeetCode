c90bbd5c94d99a0ad55af490ebfdc7522f91689c
Legend <priyanshpratham2000@gmail.com>
2026-09-03 13:52:20 +0000

    LC-smallest-integer-divisible-by-k [Runtime: N/A; Memory: N/A]

--- hash-table/smallest-integer-divisible-by-k.cpp
+++ hash-table/smallest-integer-divisible-by-k.cpp
@@ -9,12 +9,16 @@ public:
         }
         long long ans=1;
         long long c=0;
-        for(int i=0;i<10;i++){
+        for(int i=0;i<15;i++){
             c++;
             if(ans%k==0){
                 break;
             }
             ans=ans*10+1;
+            if(i==14){
+                c=-1;
+                break;
+            }
         }
         return c;
     }
