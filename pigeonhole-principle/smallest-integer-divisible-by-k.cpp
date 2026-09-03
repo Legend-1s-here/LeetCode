44132f516ab7955212500896792553adace847af
Legend <priyanshpratham2000@gmail.com>
2026-09-03 13:41:33 +0000

    LC-smallest-integer-divisible-by-k [Runtime: N/A; Memory: N/A]

---
+++ pigeonhole-principle/smallest-integer-divisible-by-k.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+        if(k%2==0){
+            return -1;
+        }
+        if(k==1){
+            return 1;
+        }
+        long long ans=1;
+        long long c=0;
+        for(int i=0;i<10;i++){
+            c++;
+            if(ans%k==0){
+                break;
+            }
+            ans=ans*10+1;
+        }
+        return c;
+    }
+};
