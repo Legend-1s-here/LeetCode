964d99cad04b2aec6aa7c5135d6985c0a2da13c6
Legend <priyanshpratham2000@gmail.com>
2026-09-03 12:40:37 +0000

    LC-reverse-integer [Runtime: N/A; Memory: N/A]

---
+++ math/reverse-integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int a=0;
+        int y=0;
+        bool is=true;
+        if(x<0){
+            x=-x;
+            is=false;
+        }
+        while(x!=0){
+            a=x%10;
+            y=y*10+a;
+            x=x/10;
+        }
+        if(is==true){
+            return y;
+        }
+        else{
+            return -y;
+        }
+    }
+};
