aefee81a736fbe89a4580b4fbcbfd616518b9b39
Legend <priyanshpratham2000@gmail.com>
2026-09-03 11:46:30 +0000

    LC-can-make-arithmetic-progression-from-sequence [Runtime: N/A; Memory: N/A]

--- sorting/can-make-arithmetic-progression-from-sequence.cpp
+++ sorting/can-make-arithmetic-progression-from-sequence.cpp
@@ -4,7 +4,7 @@ public:
         sort(arr.begin(),arr.end());
         bool is=false;
         for(int i=1;i<arr.size();i++){
-            if(arr[i]-arr[i-1]==arr[i+1]-arr[i]){
+            if((arr[i]-arr[i-1])==(arr[i+1]-arr[i])){
                 is=true;
             }
         }
