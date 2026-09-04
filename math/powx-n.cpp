12f98e7bf928ae722b16932395372e90e5cd8d67
Legend <priyanshpratham2000@gmail.com>
2026-09-04 13:38:25 +0000

    LC-powx-n [Runtime: N/A; Memory: N/A]

--- math/powx-n.cpp
+++ math/powx-n.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     double myPow(double x, int n) {
+        if(x==1){
+            return 1;
+        }
+        if(n==0){
+            return 1;
+        }
+        if(x==-1 && n%2==0){
+            return 1;
+        }
+        if(x==-1 && n%2!=0){
+            return -1;
+        }
         int bin=n;
         if(n<0){
             x=1/x;
