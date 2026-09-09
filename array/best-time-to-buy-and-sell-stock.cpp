68e962ec4e8cc72b177d3f7f95bf6c8379e2d3da
Legend <priyanshpratham2000@gmail.com>
2026-09-09 13:54:02 +0000

    LC-best-time-to-buy-and-sell-stock [Runtime: N/A; Memory: N/A]

--- array/best-time-to-buy-and-sell-stock.cpp
+++ array/best-time-to-buy-and-sell-stock.cpp
@@ -4,25 +4,15 @@ public:
         int mi=INT_MAX;
         bool is=true;
         int m=0;
+        int buy=prices[0];
+        int sell=0;
+        int cp=0;
         for(int i=0;i<prices.size();i++){
-            if((prices[i]<mi) && (i!=prices.size()-1)){
-                mi=prices[i];
-                is=false;
-            }
-            if(mi<prices[i] && is==true){
-                m=max(m,prices[i]);
-            }
-            is=true;
-            if((prices[i]<mi) && (i==prices.size()-1)){
-                return 0;
-            }
+            buy=min(buy,prices[i]);
+            sell=prices[i];
+            cp=sell-buy;
+            m=max(cp,m);
         }
-        if(m-mi>0){
-            return m-mi;
-        }
-        else{
-            return 0;
-        }
-        
+        return m;
     }
 };
