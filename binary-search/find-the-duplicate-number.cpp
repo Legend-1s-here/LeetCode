ce918ec7a201ee0c6e2242988ea28486105246ef
Legend <priyanshpratham2000@gmail.com>
2026-09-14 12:24:56 +0000

    LC-find-the-duplicate-number [Runtime: N/A; Memory: N/A]

---
+++ binary-search/find-the-duplicate-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int x=nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(x==nums[i]){
+                return x;
+            }
+            else{
+                x=nums[i];
+            }
+        }
+        return x;
+    }
+};
